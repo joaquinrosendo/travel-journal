dc15e7f2b22cd16eb43584ac11f2868770a8f260
Joaquin Rosendo <joaquinrosendo@live.com>
2023-11-03 16:12:48 -0500

    finish app

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,20 @@
 import '../styles/Card.css';
 import React from 'react';
 
-function Card(){
+function Card(props){
+    
     return(
         <div className="card">
-            <img />
+            <img src={props.imageUrl} />
             <div className='card--details'>
                 <div className="card--location">
                     <i className="card--location--pin fa-solid fa-location-dot"></i>
-                    <p className="card--location--country">Japan</p>
-                    <p className="card--location--link">View on Google Maps</p>
+                    <p className="card--location--country">{props.location}</p>
+                    <a href={props.googleMapsUrl} className="card--location--link">View on Google Maps</a>
                 </div>
-                <h2>Mount Fuji</h2>
-                <p>12 Jan, 2021 - 24 Jan, 2021</p>
-                <article>
-                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
-                </article>
+                <h2>{props.title}</h2>
+                <p>{`${props.startDate} - ${props.endDate}`}</p>
+                <article>{props.description}</article>
             </div>
         </div>
     )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,15 @@
 import '../styles/Main.css';
 import React from 'react';
 import Card from './Card';
+import data from '../data.js';
 
 function Main(){
+
+    const journalData = data.map(location => <Card key={location.id} {...location}/> )
+    
     return(
         <main className="main-container">
-            <Card />
+            {journalData}
         </main>
     )
 }
